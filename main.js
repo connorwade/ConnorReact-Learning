a327c6c0fefce7d7c46d5a8aaab35d6522cfa0fc
Connor Wade <connor.wade.ut@gmail.com>
2022-11-19 17:38:21 -0700

    step: use JS nodes to inject HTML items

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const element = `<h1 title="foo">Hello</h1>`;
+//1. We breakdown our HTML node to an JS object
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello",
+  },
+};
+//2. We grab the root of our HTML file
 const root = document.getElementById("root");
-root.innerHTML = element;
+//3. We build the node from JS object
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+//4. We set the text within the node
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+//5. We append text to the node and then we append the node to the root
+node.appendChild(text);
+root.appendChild(node);
