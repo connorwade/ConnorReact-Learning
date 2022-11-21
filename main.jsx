3577b5e26fccc14b599cc44e308419053bcbb983
Connor Wade <connor.wade.ut@gmail.com>
2022-11-20 21:05:46 -0700

    step: added workinprogress and adjusted JSX for finished dom

--- main.jsx
+++ main.jsx
@@ -1,12 +1,18 @@
 import ConnoReact from "./lib/ConnoReact";
 
 /** @jsx ConnoReact.createElement */
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
-);
+const element = () => {
+  let count = 0;
+  const setCount = (count) => count++;
+  return (
+    <div id="App">
+      <h1>Counter</h1>
+      <div className="card">
+        <button onClick={() => setCount(count)}>count is {count}</button>
+      </div>
+    </div>
+  );
+};
 
 const root = document.getElementById("root");
-ConnoReact.render(element, root);
+ConnoReact.render(element(), root);
