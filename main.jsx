aada30f514a0592c2c9215b1c18e2999b02c48f3
Connor Wade <connor.wade.ut@gmail.com>
2022-11-20 23:09:18 -0700

    step: add function components and state

--- main.jsx
+++ main.jsx
@@ -2,20 +2,20 @@ import ConnoReact from "./lib/ConnoReact";
 
 /** @jsx ConnoReact.createElement */
 function App(props) {
-  let count = 0;
-  const setCount = (count) => count++;
+  const [count, setCount] = ConnoReact.useState(1);
   return (
     <div id="App">
       <h1>{props.title}</h1>
       <div className="card">
-        <button onClick={() => setCount(count)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
       </div>
     </div>
   );
 }
-// We are now closer this implemenation
-// const element = <App title="Counter" />;
-const element = App({ title: "Counter" });
+
+const element = <App title="Counter" />;
 
 const root = document.getElementById("root");
 ConnoReact.render(element, root);
