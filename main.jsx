0ca8015cf928ac59f8282ae6e28e57887350c992
Connor Wade <connor.wade.ut@gmail.com>
2022-11-20 21:54:00 -0700

    step: add mutability to the UI

--- main.jsx
+++ main.jsx
@@ -1,18 +1,21 @@
 import ConnoReact from "./lib/ConnoReact";
 
 /** @jsx ConnoReact.createElement */
-const element = () => {
+function App(props) {
   let count = 0;
   const setCount = (count) => count++;
   return (
     <div id="App">
-      <h1>Counter</h1>
+      <h1>{props.title}</h1>
       <div className="card">
         <button onClick={() => setCount(count)}>count is {count}</button>
       </div>
     </div>
   );
-};
+}
+// We are now closer this implemenation
+// const element = <App title="Counter" />;
+const element = App({ title: "Counter" });
 
 const root = document.getElementById("root");
-ConnoReact.render(element(), root);
+ConnoReact.render(element, root);
